21688cacc7996bfe6f532587e69dc6b4fba1d0cc
James Fuller <fuller.jdf@outlook.com>
2025-09-14 20:03:53 -0500

    Started UrlController to eventually be responsible for a post endpoint and a get endpoint

--- src/main/java/com/fullersource/urlshortener/controller/UrlController.java
+++ src/main/java/com/fullersource/urlshortener/controller/UrlController.java
@@ -1,5 +1,12 @@
 package com.fullersource.urlshortener.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
 public class UrlController {
     
 }
