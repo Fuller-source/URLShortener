1a6476ffc0b7b514e6e13b3c745c988c40abb94d
James Fuller <fuller.jdf@outlook.com>
2025-09-15 14:10:03 -0500

    Completed the second API endpoint to get the shortKey's corresponding longUrl

--- src/main/java/com/fullersource/urlshortener/controller/UrlController.java
+++ src/main/java/com/fullersource/urlshortener/controller/UrlController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fullersource.urlshortener.service.UrlService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +21,12 @@ public class UrlController {
     private UrlService urlService;
 
     @PostMapping("/shorten")
-    public String listenUrl(@RequestBody String longUrl){   // Adding request body directs spring to parse the request body to get the value for longURL
+    public String getUrl(@RequestBody String longUrl){   // Adding request body directs spring to parse the request body to get the value for longURL
         return urlService.shortenUrl(longUrl);              // calls using the injected instance above
     }
 
+    @GetMapping("/{shortKey}")    // The braces tell spring that the URL is a variable
+    public String giveUrl(@PathVariable String shortKey){
+        return urlService.getLongUrl(shortKey);
+    }
 }
